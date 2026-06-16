4392edda3c4831b715880df9b9b4c1fec17d8478
rushelby <g.ruscelli@reply.it>
2026-06-16 18:06:52 +0200

    Changed simulation to include camera

--- ur_simulation_gz/launch/ur_sim_control.launch.py
+++ ur_simulation_gz/launch/ur_sim_control.launch.py
@@ -182,6 +182,10 @@ def launch_setup(context, *args, **kwargs):
         executable="parameter_bridge",
         arguments=[
             "/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock",
+            "/camera/image@sensor_msgs/msg/Image[gz.msgs.Image",
+            "/camera/depth_image@sensor_msgs/msg/Image[gz.msgs.Image",
+            "/camera/camera_info@sensor_msgs/msg/CameraInfo[gz.msgs.CameraInfo",
+            "/camera/points@sensor_msgs/msg/PointCloud2[gz.msgs.PointCloudPacked",
         ],
         output="screen",
     )
